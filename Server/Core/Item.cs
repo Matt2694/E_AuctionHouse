ad02564026e5e4c953a5051b78815f747a8c9ad4
Jonas Laursen <jona8690@edu.eal.dk>
2017-08-21 09:30:11 +0200

    AHP specifications

---
+++ Server/Core/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Core
+{
+    public class Item
+    {
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public int StartPrice { get; set; }
+		public int Price { get; set; }
+
+
+	}
+}
